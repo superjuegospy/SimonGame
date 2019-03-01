9066e32d7902584fa48f348e8dba15ba1bd064a8
Brayan <cdn.eem_1@comunidad.unam.mx>
2019-03-01 10:01:03 -0600

    Imagenes de Flechas y muestra de eleccion

--- pruebaPygame.py
+++ pruebaPygame.py
@@ -1,5 +1,5 @@
-import pygame, sys
-from pygame.locals import *
+import pygame,random,time
+
 
 pygame.init()
 
@@ -10,32 +10,55 @@ black = (0,0,0) #rgb
 white = (255,255,255)  #rgb
 red = (255,0,0)  #rgb
 
+patron = []
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Simon!')
 clock = pygame.time.Clock()
 
-flechaUp = pygame.image.load('flechaUp.png') #Solo vamos a cargar la imagen
-flechaDown = pygame.image.load('flechaDown.png') #Solo vamos a cargar la imagen
-flechaLeft = pygame.image.load('flechaLeft.png') #Solo vamos a cargar la imagen
-flechaRight = pygame.image.load('flechaRight.png') #Solo vamos a cargar la imagen
-
+listImg=[]
+listImg.append( pygame.image.load('flechaUp.png')) #Solo vamos a cargar la imagen
+listImg.append( pygame.image.load('flechaDown.png')) #Solo vamos a cargar la imagen
+listImg.append( pygame.image.load('flechaLeft.png') )#Solo vamos a cargar la imagen
+listImg.append( pygame.image.load('flechaRight.png')) #Solo vamos a cargar la imagen
 def mostrar(imagenObj,x,y):
     """ Mostrar una imagen a partir de coordenadas """
     gameDisplay.blit(imagenObj,(x,y))
+def game_loop():
+    """ Realiza el loop del juego"""
+    imagen = None
+    gameExit=False
 
-crash=False
+    while not gameExit:
+        if len(patron) == 0:
+            patron.append(random.randint(0,3))
 
-while not crash:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:#la x de la ventana
-            crash=True
-        print(event)
+        # for num in patron:
+        #     imagen = listImg[num]
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:#la x de la ventana
+                gameExit=True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    imagen = listImg[2]
+                elif event.key == pygame.K_UP:
+                    imagen = listImg[0]
+                elif event.key == pygame.K_RIGHT:
+                    imagen = listImg[3]
+                elif event.key == pygame.K_DOWN:
+                    imagen = listImg[1]
 
-    gameDisplay.fill(white)#importa orden en que pintas
-    mostrar(flechaUp,display_width*0.4,display_height*0.2)#Mostramos la flecha up
+            print(event)
 
-    pygame.display.update()#tambien se puede usra .flip() pero es para toda la pantalla
-    clock.tick(60)#Frames per second
+
+        gameDisplay.fill(white)#importa orden en que pintas
+        if imagen is not None:
+            #Mostramos la flechaa adecuada
+            mostrar(imagen,display_width*0.4,display_height*0.2)
+
+        pygame.display.update()#tambien se puede usra .flip() pero es para toda la pantalla
+        clock.tick(30)#Frames per second
+
+game_loop()
 pygame.quit()
 quit()
 
